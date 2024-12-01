4bcec541cef52a762669b4e9852a71c63e81ec50
Rubens Altimari <rubens@introspection.com.br>
2024-12-01 20:44:27 +0100

    Improving version 2 of Day 1

--- day01/s2.py
+++ day01/s2.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import csv
+import argparse
 import collections
 
-a = []
-b = []
 
-with open("input.txt") as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='\"')
-    for line in reader:
-        a.append(int(line[0]))
-        b.append(int(line[3]))
-print('%d lines' % len(a))
+def main(args):
+    a, b = get_lists(args.filename)
+    distance(a, b)
+    similarity(a, b)
 
-a.sort()
-b.sort()
+def get_lists(filename):
+    a = []
+    b = []
+    with open(filename) as lines:
+        for line in lines:
+            col1, col2 = line.split()
+            a.append(int(col1))
+            b.append(int(col2))
+    print(f'Lines read: {len(a)}\n')
+    return sorted(a), sorted(b)
 
-distance = 0
-for i in range(len(a)):
-    diff = abs(a[i] - b[i])
-    distance += diff
-    # print('%d\t%d\t%d\t%d' % (a[i], b[i], diff, distance))
+def distance(a, b):
+    distance = sum(abs(i - j) for i, j in zip(a, b))
+    print(f'Distance is {distance}\n')
 
-print('Distance is %d\n' % distance)
+def similarity(a, b):
+    a_count = collections.Counter(a)
+    b_count = collections.Counter(b)
 
-a_count = collections.Counter(a)
-b_count = collections.Counter(b)
+    # Out of curiosity, how many duplicates are there in each list?
+    print('Duplicates: %d %d\n' % (
+        sum(count for count in a_count.values() if count > 1),
+        sum(count for count in b_count.values() if count > 1)
+    ))
 
-print('Duplicates: %d %d' % (
-    sum([count for item, count in a_count.items() if count > 1]),
-    sum([count for item, count in b_count.items() if count > 1])
-))
+    similarity = sum(i * b_count[i] for i in a)
+    print(f'Similarity is {similarity}\n')
 
-similarity = 0
-for key in a:
-    score = b_count.get(key, 0)
-    similarity += key * score
-    # print('%d\t%d\t%d' % (key, score, similarity))
-
-print('Similarity is %d\n' % similarity)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help='Input file to process')
+    args = parser.parse_args()
+    main(args)
