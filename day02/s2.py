3ad2713bc4692be8e6c3db1db14ce54e0cf6525c
Rubens Altimari <rubens@introspection.com.br>
2024-12-02 15:39:32 +0100

    Simplifying ("de-efficientazing") code a little bit. Separate deltas[] was overkill.

--- day02/s2.py
+++ day02/s2.py
@@ -5,54 +5,54 @@ import argparse
 
 
 def main(args):
-    reports, deltas = read_lines(args.filename)
-    # show_reports(reports, deltas)
-    count_safes(deltas)
+    reports = read_lines(args.filename)
+    # show_reports(reports)
+    count_safes(reports)
 
-    dampen_reports(reports, deltas)
-    # show_reports(reports, deltas)
-    count_safes(deltas)
+    dampen_reports(reports)
+    # show_reports(reports)
+    count_safes(reports)
 
 def read_lines(filename):
     reports = []
-    deltas = []
     with open(filename) as lines:
         for line in lines:
-            values = [int(i) for i in line.split()]
-            reports.append(values)
-            deltas.append([values[i+1] - values[i] for i in range(len(values) - 1)])
-    return reports, deltas
+            reports.append([int(i) for i in line.split()])
+    return reports
 
-def count_safes(deltas):
-    safes = [is_safe(delta) for delta in deltas].count(True)
-    print(f'Total Reports: {len(deltas)}')
+def count_safes(reports):
+    all_deltas = [calculate_deltas(report) for report in reports]
+    safes = [is_safe(deltas) for deltas in all_deltas].count(True)
+    print(f'Total Reports: {len(reports)}')
     print(f'Safe Reports: {safes}\n')
 
-def dampen_reports(reports, deltas):
+def dampen_reports(reports):
     # This could be made more efficient by disconsidering reports that have 2+ problems in their deltas
     # (that is, 0s or just one different sign), since those can NEVER be fixed. Out of the ones that
     # have just ONE problem, there's a chance (still not guaranteed) to fix them. Also, there's probably
     # many ways to improve efficiency by analyzing the delta instead of trying by brute force: adding pairs,
     # for instance, might work.
-    for index, (report, delta) in enumerate(zip(reports, deltas)):
-        if not is_safe(delta):
-            # show_reports([report], [delta])
+    for index, report in enumerate(reports):
+        if not is_safe(calculate_deltas(report)):
+            # show_reports([report])
             for i in range(len(report)):
                 new_report = report[:i] + report[i+1:]
-                new_delta = [new_report[i+1] - new_report[i] for i in range(len(new_report) - 1)]
-                if is_safe(new_delta):
-                    # show_reports([new_report], [new_delta])
+                if is_safe(calculate_deltas(new_report)):
+                    # show_reports([new_report])
                     reports[index] = new_report
-                    deltas[index] = new_delta
                     break
             # print()
 
-def show_reports(reports, deltas):
-    for report, delta in zip(reports, deltas):
-        print(f'Safe: {is_safe(delta)} \t {report} \t {delta}')
+def show_reports(reports):
+    for report in reports:
+        deltas = calculate_deltas(report)
+        print(f'Safe: {is_safe(deltas)} \t {report} \t {deltas}')
 
-def is_safe(delta):
-    return all(1 <= i <= 3 for i in delta) or all(-3 <= i <= -1 for i in delta)
+def calculate_deltas(report):
+    return [report[i+1] - report[i] for i in range(len(report)-1)]
+
+def is_safe(deltas):
+    return all(1 <= i <= 3 for i in deltas) or all(-3 <= i <= -1 for i in deltas)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
