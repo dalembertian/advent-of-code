9d71e6bffb3726ea21e2d3a1d9ee0f97abf1cde8
Rubens Altimari <rubens@introspection.com.br>
2024-12-03 18:05:37 +0100

    Starting to attack Part 2 - but brute force will not cut it

--- 201519/solve.py
+++ 201519/solve.py
@@ -9,9 +9,10 @@ import argparse
 def main(args):
     subs, molecule = read_lines(args.filename)
     print(f'Molecule: {molecule}\n')
-    print(f'Calibration: {len(calibrate(molecule, subs))}')
+    print(f'Calibration: {len(makes_all_subs(molecule, subs))}')
 
-    recipes = find_
+    recipes = generate_molecules('e', molecule, subs)
+    print(f'{len(recipes)} Recipe(s) found, minimum steps required is: {min(recipes) if recipes else None}')
 
 def read_lines(filename):
     # subs = {}
@@ -26,10 +27,9 @@ def read_lines(filename):
             else:
                 break
         molecule = lines.readline().strip()
-    print(subs)
     return subs, molecule
 
-def sub(molecule, substring, replacement):
+def makes_one_sub(molecule, substring, replacement):
     start = -1
     molecules = []
     subs = molecule.count(substring)
@@ -38,11 +38,36 @@ def sub(molecule, substring, replacement):
         molecules.append(molecule[:start] + replacement + molecule[start+len(substring):])
     return molecules
 
-def calibrate(molecule, subs):
+def makes_all_subs(molecule, subs):
     molecules = []
     for substring, replacement in subs:
-        molecules.extend(sub(molecule, substring, replacement))
+        molecules.extend(makes_one_sub(molecule, substring, replacement))
     return set(molecules)
+
+def generate_molecules(start, target, subs, find_all=False):
+    # TODO: log the recipe steps, so the molecule can be reproduced! :-)
+    recipes = []
+    target_size = len(target)
+
+    level = (start)
+    level_number = 1
+    while level:
+        new_level = []
+        for m in level:
+            new_molecules = makes_all_subs(m, subs)
+            for new_m in new_molecules:
+                if new_m == target:
+                    recipes.append(level_number)
+                    if not find_all:
+                        return recipes
+                elif len(new_m) < target_size:
+                    new_level.append(new_m)
+        level = set(new_level)
+        level_number += 1
+        # print(level_number)
+        # print([level])
+
+    return recipes
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
