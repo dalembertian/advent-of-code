c327b37069b4b0d57deee1bc190be74d7f801192
Rubens Altimari <rubens@introspection.com.br>
2024-12-04 20:36:38 +0100

    Day 4, Part 2

--- 202404/solve.py
+++ 202404/solve.py
@@ -7,7 +7,8 @@ import re
 
 def main(args):
     lines = read_lines(args.filename)
-    print(f'Part 1 - XMAS instances: {find_word("XMAS", lines)}')
+    print(f'Part 1 - XMAS instances: {find_word('XMAS', lines)}')
+    print(f'Part 2 - X-MAS instances: {find_x_word("MAS", lines)}')
 
 def read_lines(filename):
     with open(filename) as lines:
@@ -43,6 +44,43 @@ def generate_strings_from_diagonals(lines):
 def generate_strings_from_other_diagonals(lines):
     inverted_lines = [line[::-1] for line in lines]
     return generate_strings_from_diagonals(inverted_lines)
+
+def find_x_word(word, lines):
+    # First, replace MAS by its "marked" version, "M.S" on the first half of diagonals
+    marked_word = word[:len(word)//2] + '-' + word[len(word)//2+1:]
+    strings = generate_strings_from_diagonals(lines)
+    strings = mark_x_word(word, marked_word, strings)
+    strings = mark_x_word(word[::-1], marked_word[::-1], strings)
+
+    # Now rebuild the lines out of the marked strings (yeah, looks awful)
+    rows = len(lines)
+    cols = len(lines[0])
+    matrix = [['' for j in range(cols)] for i in range(rows)]
+    for i, line in enumerate(strings):
+        for j, c in enumerate(line):
+            matrix[min(i,rows-1)-j][i-min(i,rows-1)+j] = c
+    marked_lines = [''.join(line) for line in matrix]
+
+    # Finally, look for the marked version on the OTHER diagonals
+    strings = generate_strings_from_other_diagonals(marked_lines)
+    marked_word_regex = re.compile(marked_word)
+    count  = sum(len(marked_word_regex.findall(string)) for string in strings)
+    count += sum(len(marked_word_regex.findall(string[::-1])) for string in strings)
+    return count
+
+def mark_x_word(word, marked_word, lines):
+    word_regex = re.compile('('+word+')')
+    new_lines = []
+    for line in lines:
+        segs = word_regex.split(line)
+        new_segs = []
+        for seg in segs:
+            if seg == word:
+                new_segs.append(marked_word)
+            else:
+                new_segs.append(seg)
+        new_lines.append(''.join(new_segs))
+    return new_lines
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
