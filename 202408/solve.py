cb335f3ce63307b9e590eb64909c52159ce96ac2
Rubens Altimari <rubens@introspection.com.br>
2024-12-08 10:56:54 +0100

    Day 8, Part 2

--- 202408/solve.py
+++ 202408/solve.py
@@ -10,15 +10,27 @@ def main(args):
     antinodes = set(count_antinodes(antennas, length, width))
     print(f'Part 1 - total (unique) antinodes: {len(antinodes)}')
 
+    antinodes = set(count_antinodes(antennas, length, width, True))
+    print(f'Part 2 - total (unique) antinodes: {len(antinodes)}')
+
     plot(antennas, antinodes, length, width)
 
-def count_antinodes(antennas, length, width):
+def count_antinodes(antennas, length, width, harmonics=False):
     antinodes = []
     for frequency, positions in antennas.items():
         for i, a in enumerate(positions[:-1]):
             for b in positions[i+1:]:
                 dx, dy = b[0] - a[0], b[1] - a[1]
                 nodes = [(a[0]-dx, a[1]-dy), (b[0]+dx, b[1]+dy)]
+                if harmonics:
+                    x, y = a[0], a[1]
+                    while 0 <= x < length and 0 <= y < width:
+                        nodes.append((x,y))
+                        x, y = x - dx, y - dy
+                    x, y = a[0], a[1]
+                    while 0 <= x < length and 0 <= y < width:
+                        nodes.append((x,y))
+                        x, y = x + dx, y + dy
                 antinodes.extend([n for n in nodes if 0 <= n[0] < length and 0 <= n[1] < width])
     return antinodes
 
