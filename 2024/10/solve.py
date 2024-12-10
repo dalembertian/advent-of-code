68d3337bbd39e0e6fc954370a8a35026c1d99617
Rubens Altimari <rubens@introspection.com.br>
2024-12-10 11:11:00 +0100

    Day 10, Part 2

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -3,40 +3,37 @@
 
 import argparse
 
-# x, y, next
-MOVEMENTS = {
-    '^': (-1,  0, '>'),
-    '>': ( 0,  1, 'v'),
-    'v': ( 1,  0, '<'),
-    '<': ( 0, -1, '^'),
-}
-
 def main(args):
     maze = read_lines(args.filename)
-    plot(maze)
+    # plot(maze)
 
     trailheads = find_hikes(maze)
     print(f'Part 1 - Total sum of trailheads is: {sum(trailheads)}')
 
-def find_hikes(maze):
+    trailheads = find_hikes(maze, unique=False)
+    print(f'Part 2 - Total sum of SCORE of trailheads is: {sum(trailheads)}')
+
+def find_hikes(maze, unique=True):
     heads = []
     size = len(maze) - 2
     for row in range(size):
         for col in range(size):
-            hike = check_trail(row, col, maze)
-            if hike:
-                heads.append(hike)
+                if unique:
+                    heads.append(len(set(check_trail(row, col, maze))))
+                else:
+                    heads.append(len(check_trail(row, col, maze)))
     return heads
 
 def check_trail(row, col, maze, expected=0):
     x, y = row + 1, col + 1
-    print(''.join([' ' for i in range(expected)]), row, col, maze[x][y], expected)
-    if maze[x][y] != expected or maze[x][y] == -1:
-        return 0
+    # print(''.join([' ' for i in range(expected)]), x, y, maze[x][y], expected)
+    if maze[x][y] != expected or maze[x][y] == '.':
+        return []
     elif maze[x][y] == expected:
         if expected == 9:
-            return 1
+            return [(row,col)]
         else:
+            # TODO: no need to check where we came from
             return check_trail(row - 1, col, maze, expected + 1) + \
                    check_trail(row + 1, col, maze, expected + 1) + \
                    check_trail(row, col - 1, maze, expected + 1) + \
@@ -44,14 +41,14 @@ def check_trail(row, col, maze, expected=0):
 
 def plot(maze):
     for row in maze:
-        print(row)
+        print(''.join([str(c) for c in row]))
 
 def read_lines(filename):
     maze = []
     with open(filename) as lines:
         for line in lines:
-            maze.append([-1] + [int(p) for p in line.strip()] + [-1])
-    border = [-1 for i in range(len(maze[0]))]
+            maze.append(['.'] + [int(p) for p in line.strip()] + ['.'])
+    border = ['.' for i in range(len(maze[0]))]
     maze.insert(0, border)
     maze.append(border[:])
     return maze
