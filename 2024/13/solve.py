c7f21ed636dc3a44abcb6a53471c1258ed139cf2
Rubens Altimari <rubens@introspection.com.br>
2024-12-13 12:23:10 +0100

    Day 13, Part 1

---
+++ 2024/13/solve.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+import re
+from collections import defaultdict
+        
+DIRECTIONS = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+def main(args):
+    machines = read_lines(args.filename)
+    # print(machines)
+
+    prizes = []
+    for a, b, prize in machines:
+        factors = set(factor(prize[0], a[0], b[0]))
+        factors = factors.intersection(factor(prize[1], a[1], b[1]))
+        if factors:
+            prizes.append(min([3*x + y for x, y in factors]))
+    # print(prizes)
+    print(sum(prizes))
+
+
+def factor(number, a, b):
+    factors = []
+    i, factor = 0, 0
+    while i <= 100 and factor <= number:
+        rest = number - factor
+        if b <= rest and rest % b == 0:
+            j = rest // b
+            if b <= 100:
+                factors.append((i, j))
+        i, factor = i + 1, factor + a
+    return factors
+
+def read_lines(filename):
+    values = re.compile(r'.*X[+|=](\d+), Y[+|=](\d+)')
+    machines = []
+    with open(filename) as input:
+        lines = input.readlines()
+    lines.insert(0, '')
+    while lines:
+        lines.pop(0)
+        machines.append((
+            tuple([int(e) for e in values.search(lines.pop(0)).groups()]),
+            tuple([int(e) for e in values.search(lines.pop(0)).groups()]),
+            tuple([int(e) for e in values.search(lines.pop(0)).groups()]),
+        ))
+    return machines
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help='Input file to process')
+    args = parser.parse_args()
+    main(args)
