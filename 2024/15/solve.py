9e655cb1f0925ab5fc48fe20c75d772dd64f9286
Rubens Altimari <rubens@introspection.com.br>
2024-12-15 10:33:28 +0100

    Day 15, Part 1

--- 2024/15/solve.py
+++ 2024/15/solve.py
@@ -2,19 +2,53 @@
 # encoding: utf-8
 
 import argparse
+import re
 
 MOVEMENTS = {
-    '^': (-1,  0),
-    '>': ( 0,  1),
-    'v': ( 1,  0),
-    '<': ( 0, -1),
+    '^': ( 0, -1),
+    '>': ( 1,  0),
+    'v': ( 0,  1),
+    '<': (-1,  0),
 }
 
 
 def main(args):
     maze, moves = read_lines(args.filename)
+
+    walk_robot(maze, moves)
     plot(maze)
-    print(moves)
+    boxes = find_element('O', maze)
+    print(f'Part 1 - GPS coordinates sum is: {sum([x + 100*y for x, y in boxes])}')
+
+def walk_robot(maze, moves):
+    x, y = find_element('@', maze)[0]
+    for move in moves:
+        dx, dy = MOVEMENTS[move]
+        # print(f'({x},{y}) -> ({x+dx},{x+dy})')
+        if maze[y+dy][x+dx] == '.' or push_box(x+dx, y+dy, move, maze):
+            x, y = move_element('@', x, y, move, maze)
+
+def push_box(x, y, move, maze):
+    if maze[y][x] == 'O':
+        dx, dy = MOVEMENTS[move]
+        if maze[y+dy][x+dx] == '.':
+            move_element('O', x, y, move, maze)
+            return True
+        elif maze[y+dy][x+dx] == 'O':
+            if push_box(x+dx, y+dy, move, maze):
+                move_element('O', x, y, move, maze)
+                return True
+        else:
+            return False
+
+def find_element(symbol, maze):
+    return [(m.start(), j) for j, line in enumerate(maze) for m in re.finditer(symbol, ''.join(line))]
+
+def move_element(symbol, x, y, move, maze):
+    dx, dy = MOVEMENTS[move]
+    maze[y][x] = '.'
+    maze[y+dy][x+dx] = symbol
+    return x+dx, y+dy
 
 def plot(maze):
     print(f'    {''.join([str(i // 10) if i >= 10 else ' ' for i in range(len(maze[0]))])}')
