23a8cfe484a3effa5154a0cc3c735e870b8ec66b
Rubens Altimari <rubens@introspection.com.br>
2024-12-17 20:02:46 +0100

    Well, too much recursion

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
 import argparse
 import re
 
@@ -20,15 +21,20 @@ OPPOSITE = {
     'v': '^',
     '<': '>',
 }
+INFINITE = float('inf')
 
 
 def main(args):
+    print(sys.getrecursionlimit())
+    sys.setrecursionlimit(3000)
+    print(sys.getrecursionlimit())
+
     maze = read_lines(args.filename)
 
     nodes  = defaultdict(dict)
     start  = find_element('S', maze)[0]
     finish = find_element('E', maze)[0]
-    find_path(start, '>', start, '', maze, nodes)
+    find_path(start, '>', start, 0, '', maze, nodes)
 
     for k in nodes.keys():
         print(f'{k}: {nodes[k]}')
@@ -52,51 +58,49 @@ def main(args):
 
 def dijkstra(start, finish, nodes, maze):
     # Mark all unvisited nodes with distance "infinite" from start - except start
-    infinite = float('inf')
     unvisited = list(nodes.keys())
     for node in unvisited:        
-        nodes[node]['cost'] = infinite
+        nodes[node]['cost'] = INFINITE
     nodes[start]['cost'] = 0
 
     while unvisited:
         # Get unvisited node with shortest distance to start
         unvisited.sort(key=lambda n: nodes[n]['cost'])
-        plot(maze)
-        print(unvisited)
+        # plot(maze)
+        # print(unvisited)
         this = unvisited.pop(0)
-        if nodes[this]['cost'] == infinite:
+        if nodes[this]['cost'] == INFINITE:
             break
 
         # Visits every node linked to this STILL IN unvisited
-        print(this)
-        for v, path in nodes[this]['nodes']:
+        # print(this)
+        for v, (cost, path) in nodes[this]['nodes'].items():
             if v in unvisited:
-                print(v, nodes[v].get('prev_node', None), nodes[v].get('cost', None), end='')
-                cost = nodes[this]['cost'] + cost_path(path)
-                if cost < nodes[v]['cost']:
-                    nodes[v]['cost'] = cost
+                # print(v, nodes[v].get('prev_node', None), nodes[v].get('cost', None), end='')
+                new_cost = nodes[this]['cost'] + cost
+                if new_cost < nodes[v]['cost']:
+                    nodes[v]['cost'] = new_cost
                     nodes[v]['prev_path'] = invert_path(path)
                     nodes[v]['prev_node'] = this
-                    print(' -> ', nodes[v]['prev_node'], nodes[v]['cost'], end='')
-                print()
-        print()
-        input()
+        #             print(' -> ', nodes[v]['prev_node'], nodes[v]['cost'], end='')
+        #         print()
+        # print()
+        # input()
     if not unvisited:
         return True
 
-def find_path(visit, move, previous, path, maze, nodes):
+def find_path(visit, move, previous, initial_cost, path, maze, nodes):
     x, y = visit
     path += move
 
-    # for k in nodes.keys():
-    #     print(f'{k}: {nodes[k]}')
-    # plot(maze, visit, move)
-    # print(f'From: {previous}')
-    # input()
+    for k in nodes.keys():
+        print(f'{k}: {nodes[k]}')
+    plot(maze, visit, move)
+    print(f'From: {previous}')
+    input()
 
     if maze[y][x] in (' ', 'E'):
-        nodes[previous].setdefault('nodes', []).append((visit, path))
-        nodes[visit].setdefault('nodes', []).append((previous, invert_path(path)))
+        add_node(visit, previous, initial_cost, path, nodes)
         return
 
     if maze[y][x] == 'S':
@@ -105,15 +109,25 @@ def find_path(visit, move, previous, path, maze, nodes):
     maze[y][x] = ' '
     moves = [m for m, (dx, dy) in MOVEMENTS.items() if m != OPPOSITE[move] and maze[y+dy][x+dx] != '#']
     if len(moves) > 1 and path:
-        nodes[previous].setdefault('nodes', []).append((visit, path))
-        nodes[visit].setdefault('nodes', []).append((previous, invert_path(path)))
+        add_node(visit, previous, initial_cost, path, nodes)
         previous = visit
         path = ''
 
     for m in moves:
         dx, dy = MOVEMENTS[m]
         if m != OPPOSITE[move] and maze[y+dy][x+dx] != '#':
-            find_path((x+dx, y+dy), m, previous, path, maze, nodes)
+            cost = initial_cost if path != '' else (0 if m == move else 1000)
+            find_path((x+dx, y+dy), m, previous, cost, path, maze, nodes)
+
+def add_node(this, previous, initial_cost, path, nodes):
+    new_cost = initial_cost + cost_path(path)
+    prev_nodes = nodes[previous].setdefault('nodes', {})
+    this_nodes = nodes[this].setdefault('nodes', {})
+    # Only add a node if it's shorter then previous parallel one
+    old_cost, old_path = this_nodes.get(previous, (INFINITE, ''))
+    if new_cost < old_cost:
+        nodes[previous]['nodes'][this] = (new_cost, path)
+        nodes[this]['nodes'][previous] = (new_cost, invert_path(path))
 
 def invert_path(path):
     return ''.join([OPPOSITE[move] for move in path[::-1]])
