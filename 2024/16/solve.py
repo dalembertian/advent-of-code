604572854c735ec7ba08869d5ae7d5426fcda015
Rubens Altimari <rubens@introspection.com.br>
2024-12-17 22:29:27 +0100

    Cleaning up a bit

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys
 import argparse
 import re
 
-#from bisect import insort
-# insort(list, element)
 from collections import defaultdict
 
 MOVEMENTS = {
@@ -25,10 +22,6 @@ INFINITE = float('inf')
 
 
 def main(args):
-    print(sys.getrecursionlimit())
-    sys.setrecursionlimit(3000)
-    print(sys.getrecursionlimit())
-
     maze = read_lines(args.filename)
 
     nodes  = defaultdict(dict)
@@ -38,66 +31,22 @@ def main(args):
 
     for k in nodes.keys():
         print(f'{k}: {nodes[k]}')
+    print()
 
-    # for node in nodes.keys():
-    #     for end, path in nodes[node]['nodes']:
-    #         if path:
-    #             plot(maze, node, path)
-    #             print(node, path)
-    #             input()
-    if dijkstra(start, finish, nodes, maze):
-        node = finish
-        path = ''
-        while node != start:
-            print(node, nodes[node]['prev_node'])
-            path += nodes[node]['prev_path']
-            node  = nodes[node]['prev_node']
-        path = invert_path(path)
+    path, cost = dijkstra(start, nodes, maze)
+    if path:
         plot(maze, start, path)
-        print(f'Cost: {cost_path(path)}')
+        print(f'Cost by Dijkstra: {cost}')
 
-def dijkstra(start, finish, nodes, maze):
-    # Mark all unvisited nodes with distance "infinite" from start - except start
-    unvisited = list(nodes.keys())
-    for node in unvisited:        
-        nodes[node]['cost'] = INFINITE
-    nodes[start]['cost'] = 0
-
-    while unvisited:
-        # Get unvisited node with shortest distance to start
-        unvisited.sort(key=lambda n: nodes[n]['cost'])
-        # plot(maze)
-        # print(unvisited)
-        this = unvisited.pop(0)
-        if nodes[this]['cost'] == INFINITE:
-            break
-
-        # Visits every node linked to this STILL IN unvisited
-        # print(this)
-        for v, (cost, path) in nodes[this]['nodes'].items():
-            if v in unvisited:
-                # print(v, nodes[v].get('prev_node', None), nodes[v].get('cost', None), end='')
-                new_cost = nodes[this]['cost'] + cost
-                if new_cost < nodes[v]['cost']:
-                    nodes[v]['cost'] = new_cost
-                    nodes[v]['prev_path'] = invert_path(path)
-                    nodes[v]['prev_node'] = this
-        #             print(' -> ', nodes[v]['prev_node'], nodes[v]['cost'], end='')
-        #         print()
-        # print()
-        # input()
-    if not unvisited:
-        return True
-
-def find_path(visit, move, previous, initial_cost, path, maze, nodes):
+def find_path(visit, move, previous, initial_cost, path, maze, nodes, segments=[]):
     x, y = visit
     path += move
 
-    for k in nodes.keys():
-        print(f'{k}: {nodes[k]}')
-    plot(maze, visit, move)
-    print(f'From: {previous}')
-    input()
+    # for k in nodes.keys():
+    #     print(f'{k}: {nodes[k]}')
+    # plot(maze, visit, move)
+    # print(f'From: {previous}')
+    # input()
 
     if maze[y][x] in (' ', 'E'):
         add_node(visit, previous, initial_cost, path, nodes)
@@ -142,6 +91,49 @@ def cost_path(path):
 
 def find_element(symbol, maze):
     return [(m.start(), j) for j, line in enumerate(maze) for m in re.finditer(symbol, ''.join(line))]
+
+def dijkstra(start, nodes, maze):
+    # Mark all unvisited nodes with distance "infinite" from start - except start
+    unvisited = list(nodes.keys())
+    for node in unvisited:
+        nodes[node]['cost'] = INFINITE
+    nodes[start]['cost'] = 0
+
+    while unvisited:
+        # Get unvisited node with shortest distance to start
+        unvisited.sort(key=lambda n: nodes[n]['cost'])
+        # plot(maze)
+        # print(unvisited)
+        this = unvisited.pop(0)
+        if nodes[this]['cost'] == INFINITE:
+            break
+
+        # Visits every node linked to this STILL IN unvisited
+        # print(this)
+        for v, (cost, path) in nodes[this]['nodes'].items():
+            if v in unvisited:
+                # print(v, nodes[v].get('prev_node', None), nodes[v].get('cost', None), end='')
+                new_cost = nodes[this]['cost'] + cost
+                if new_cost < nodes[v]['cost']:
+                    nodes[v]['cost'] = new_cost
+                    nodes[v]['prev_path'] = invert_path(path)
+                    nodes[v]['prev_node'] = this
+        #             print(' -> ', nodes[v]['prev_node'], nodes[v]['cost'], end='')
+        #         print()
+        # print()
+        # input()
+    if unvisited:
+        return '', 0
+
+    # Builds path from finish to start
+    finish = find_element('E', maze)[0]
+    node = finish
+    path = ''
+    while node != start:
+        # print(node, nodes[node]['prev_node'])
+        path += nodes[node]['prev_path']
+        node  = nodes[node]['prev_node']
+    return invert_path(path), nodes[finish]['cost']
 
 def plot(maze, start=None, path=None):
     cost = 0
