1d80df23b53965cc159fb60c567d2cc1178d0ff3
Rubens Altimari <rubens@introspection.com.br>
2024-12-26 12:10:47 +0100

    Trying to cope with 25 pesky robots

--- 2024/21/solve.py
+++ 2024/21/solve.py
@@ -23,30 +23,53 @@ def main(args):
     # 185256 - too high
     total = 0
     for code in door_codes:
-        best_r3 = ''
-        r1s = keypad_strokes(code, 'A', NUM_KEYPAD)
-        for r1 in r1s:
-            r2s = keypad_strokes(r1, 'A', DIR_KEYPAD)
-            for r2 in r2s:
-                r3s = keypad_strokes(r2, 'A', DIR_KEYPAD)
-                for r3 in r3s:
-                    if len(r3) < len(best_r3) or not best_r3:
-                        best_r3 = r3
+        best = ''
+        r1s = all_keypad_strokes(code, 'A', NUM_KEYPAD)
+        # r1s = first_keypad_strokes(code, 'A', NUM_KEYPAD)
+
+        # for i in range(24):
+        #     r1s = first_keypad_strokes(r1s, 'A', DIR_KEYPAD)
+        # r1s = [r1s]
+
+        print(r1s)
+        for i in range(0):
+            r1s.sort(key=lambda s: len(s))
+            r1s = [r for r in r1s if len(r) == len(r1s[0])]
+            r2s = []
+            for r1 in r1s:
+                r2s.extend(all_keypad_strokes(r1, 'A', DIR_KEYPAD))
+            r1s = r2s
+
+        for i in range(25):
+            r1s = [first_keypad_strokes(r1, 'A', DIR_KEYPAD) for r1 in r1s]
+            print(i)
+        r2s = r1s
+
+        for r2 in r2s:
+            if len(r2) < len(best) or not best:
+                best = r2
 
         code_num = int(re.search(r'(\d+)A*', code).group(1))
-        code_len = len(best_r3)
+        code_len = len(best)
         complexity = code_num * code_len
         total += complexity
-        print(f'code: {code} - {code_num:5} x {code_len:3} = {complexity:8} {''.join(best_r3)}')
+        print(f'code: {code} - {code_num:5} x {code_len:3} = {complexity:8} {''.join(best)}')
     print()
     print(f'Part 1 - Complexity: {total}')
 
+def first_keypad_strokes(code, start, keypad):
+    strokes = ''
+    for symbol in code:
+        strokes += keypad[(start, symbol)][0]
+        strokes += 'A'
+        start = symbol
+    return strokes
 
-def keypad_strokes(code, start, keypad):
+def all_keypad_strokes(code, start, keypad):
     if code:
         options = []
         for path in keypad[(start, code[0])]:
-            options.extend([path + 'A' + p for p in keypad_strokes(code[1:], code[0], keypad)])
+            options.extend([path + 'A' + p for p in all_keypad_strokes(code[1:], code[0], keypad)])
         return options
     else:
         return ['']
