452451fe7495782a57c257d2458d310cbf162462
Rubens Altimari <rubens@introspection.com.br>
2024-12-27 19:40:11 +0100

    Day 22, Part 2

--- 2024/22/solve.py
+++ 2024/22/solve.py
@@ -4,17 +4,47 @@
 import argparse
 import re
 
+
 def main(args):
     secrets = read_lines(args.filename)
+    amount  = int(args.number)
     
+    total, seqs, difs = generate(secrets, amount)
+    print(f'Part 1 - Sum of all {amount} secrets: {total}')
+
+    bananas = assess_offers(seqs, difs, amount)
+    print(f'Part 2 - Most amount of bananas: {bananas[-1]}')
+
+def assess_offers(seqs, difs, amount):
+    # Assess all (amount-1) offers per buyer, tabulating the first one
+    offers = {}
+    buyers = len(seqs)
+    for i in range(1, amount-2):
+        for j, (seq, dif) in enumerate(zip(seqs, difs)):
+            key = tuple(dif[i:i+4])
+            val = offers.setdefault(key, [''] * buyers)
+            if val[j] == '':
+                val[j] = seq[i+3]
+    return sorted([sum([i for i in v if i != '']) for v in offers.values()])
+
+def generate(secrets, amount):
+    # For each buyer, calculates secrets #amount times, and
+    # returns seq with last-digits, and differences
+    seqs = []
+    difs = []
     total = 0
     for secret in secrets:
+        seq = [secret % 10]
+        dif = [0]
         n = secret
-        for i in range(2000):
+        for i in range(amount):
             n = next_secret(n)
+            seq.append(n % 10)
+            dif.append(seq[i+1] - seq[i])
         total += n
-        # print(f'{secret}: {n}')
-    print(f'Part 1 - Sum of all 2000th Secrets: {total}')
+        seqs.append(seq)
+        difs.append(dif)
+    return total, seqs, difs
 
 def next_secret(secret):
     secret = prune(mix(secret, secret * 64))
@@ -37,5 +67,6 @@ def read_lines(filename):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help='Input file to process')
+    parser.add_argument("--number", "-n", default="2000", help="Number of secrets to generate (def: 2000)")
     args = parser.parse_args()
     main(args)
