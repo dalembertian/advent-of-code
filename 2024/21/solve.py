0e675c72d27848631dd4ea43f9805d9710a7d24e
Rubens Altimari <rubens@introspection.com.br>
2024-12-31 01:46:30 +0100

    Removing all the testing scaffolding

--- 2024/21/solve.py
+++ 2024/21/solve.py
@@ -3,63 +3,34 @@
 
 import argparse
 import re
-import time
 
 from keypads import *
 
 
 def main(args):
     door_codes = read_lines(args.filename)
-    s1, s2, s3 = args.step1, args.step2, args.step3
 
     setup_keypads()
-    # for k in sorted(DIR_KEYPAD.keys()):
-    #     print(k, DIR_KEYPAD[k])
 
-    # print(f'Part 1 - Complexity: {run_steps(2, 0, 0, door_codes)}')
+    print(f'Part 1 - Complexity: {run_steps(door_codes, 2)}')
 
-    print(f'Part 2 - Complexity: {run_steps(s1, s2, s3, door_codes)}')
 
-def run_steps(s1, s2, s3, door_codes):
-    start = time.time()
+def run_steps(door_codes, robots):
     total = 0
     for code in door_codes:
         combos = all_keypad_strokes(code, 'A', NUM_KEYPAD)
-        original = combos[:]
-
-        # Step 1 - first combination at hand
-        for i in range(s1):
-            print(f'Step 1, Loop {i:2}, {sum([len(combo) for combo in combos])}, t={time.time()-start:.2f}s')
+        for i in range(robots):
+            # For 2 robots, both approaches work: taking the first sequence of strokes, or...
             combos = [first_keypad_strokes(combo, 'A', DIR_KEYPAD) for combo in combos]
-
-        # Step 2 - all possible combinations
-        for i in range(s2):
-            print(f'Step 2, Loop {i:2}, {sum([len(combo) for combo in combos])}')
-            # Consider only the shortest stroke sequences
-            combos.sort(key=lambda s: len(s))
-            combos = [r for r in combos if len(r) == len(combos[0])]
-            r2s = []
-            for combo in combos:
-                r2s.extend(all_keypad_strokes(combo, 'A', DIR_KEYPAD))
-            combos = r2s
-
-        # Step 3 - like Step 1, but after choosing the shortest combo so far
-        if s3:
-            best = min(combos, key=lambda c: len(c))
-            combos = [best]
-            # print(f'Best ({len(best)}): {original[combos.index(best)]}')
-            for i in range(s3):
-                print(f'Step 3, Loop {i:2}, {sum([len(combo) for combo in combos])}, t={time.time()-start:.2f}s')
-                combos = [first_keypad_strokes(combo, 'A', DIR_KEYPAD) for combo in combos]
-            print(f'Final Sizes      {[len(combo) for combo in combos]}, t={time.time()-start:.2f}s')
-
+            # ...looking for the best. But the latter is WAY slower.
+            # metacombos = []
+            # for combo in combos:
+            #     metacombos.extend(all_keypad_strokes(combo, 'A', DIR_KEYPAD))
+            # combos = metacombos
         best = min(combos, key=lambda c: len(c))
         code_num = int(re.search(r'(\d+)A*', code).group(1))
         code_len = len(best)
-        complexity = code_num * code_len
-        total += complexity
-        print(f'code: {code} - {code_num:5} x {code_len:3} = {complexity:8}')
-        print()
+        total += code_num * code_len
     return total
 
 def read_lines(filename):
@@ -69,8 +40,5 @@ def read_lines(filename):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help='Input file to process')
-    parser.add_argument("--step1", "-s1", type=int, default=2, help='Step 1 attempts - first at hand (default: 2)')
-    parser.add_argument("--step2", "-s2", type=int, default=0, help='Step 2 attempts - all possible (default: 0)')
-    parser.add_argument("--step3", "-s3", type=int, default=0, help='Step 3 attempts - first at hand (default: 0)')
     args = parser.parse_args()
     main(args)
