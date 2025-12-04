76b2b3402387a3d806b2266cf0ada04d19266da8
Rubens Altimari <rubens@introspection.com.br>
2025-12-04 11:55:58 +0100

    Refactoring to an actual matrix

--- 2025/04/solve.py
+++ 2025/04/solve.py
@@ -4,24 +4,33 @@
 import argparse
 
 def main(args):
-    rows, width = read_lines(args.filename)
+    rows = read_lines(args.filename)
 
-    accessibles = 0
-    for i in range(1, len(rows) -1):
-        for j in range(1, width + 1):
-            if accessible(rows, i, j):
-                accessibles += 1
+    accessibles = check_accessibles(rows)
 
     # Correct: 1384
-    print(f'Part 1 - accessible rolls: {accessibles}')
+    print(f'Part 1 - accessible rolls: {len(accessibles)}')
+
+def check_accessibles(rows):
+    accessibles = []
+    for i in range(1, len(rows) -1):
+        for j in range(1, len(rows[i]) - 1):
+            if accessible(rows, i, j):
+                accessibles.append((i,j))
+
+    for pair in accessibles:
+        rows[i][j] = '.'
+    for row in rows:
+        print(row)
+    return accessibles
 
 def accessible(rows, i, j):
-    if rows[i][j] == '.':
+    if rows[i][j] != '@':
         return False
     blocks = 0
     for k in range(-1, 2):
         for l in range(-1, 2):
-            if (k != 0 or l != 0) and rows[i+k][j+l] != '.':
+            if (k != 0 or l != 0) and rows[i+k][j+l] == '@':
                 blocks += 1
                 if blocks > 3:
                     return False
@@ -31,12 +40,11 @@ def read_lines(filename):
     rows = []
     with open(filename) as lines:
         for line in lines:
-            rows.append('.'+line.strip()+'.')
-    width = len(rows[1]) - 2
-    boundary = '.' * (width + 2)
-    rows.insert(0, boundary)
-    rows.append(boundary)
-    return rows, width
+            rows.append(list('.'+line.strip()+'.'))
+    boundary = '.' * len(rows[1])
+    rows.insert(0, list(boundary))
+    rows.append(list(boundary))
+    return rows
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
