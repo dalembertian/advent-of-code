e82b7687e052801e435f93d3d0687bfeb9b561c9
Rubens Altimari <rubens@introspection.com.br>
2025-12-05 13:05:28 +0100

    Day 5, Part 1

---
+++ 2025/05/solve.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def main(args):
+    freshes, ingredients = read_lines(args.filename)
+    
+    root = TreeNode(freshes[0])
+    for fresh in freshes:
+        a, b = fresh
+        insert_node(a, b, root)
+
+    # Correct: 567
+    print(f'Part 1 - fresh ingredients: {sum([find_element(i, root) for i in ingredients])}')
+
+
+def find_element(e, node):
+    if not node:
+        return False
+    a, b = node.data
+    if e >= a and e <=b:
+        return True
+    if e < a:
+        return find_element(e, node.left)
+    else:
+        return find_element(e, node.right)
+
+
+def insert_node(a, b, parent):
+    pa, pb = parent.data
+    if a >= pa and b <= pb:
+        return
+    if a < pa:
+        if parent.left:
+            insert_node(a, b, parent.left)
+        else:
+            parent.left = TreeNode((a,b))
+    if b > pb:
+        if parent.right:
+            insert_node(a, b, parent.right)
+        else:
+            parent.right = TreeNode((a,b))
+
+
+def show_tree(node, level=0, prefix=''):
+    if node:
+        print(f'{' '*level}{prefix}', end=' ')
+        print(node.data)
+        show_tree(node.left, level+1, 'L')
+        show_tree(node.right, level+1, 'R')
+
+
+def read_lines(filename):
+    freshes = []
+    ingredients = []
+    with open(filename) as lines:
+        while line := lines.readline().strip():
+            a, b = line.split('-')
+            freshes.append((int(a), int(b)))
+        while line := lines.readline():
+            ingredients.append(int(line))
+    return freshes, ingredients
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help='Input file to process')
+    args = parser.parse_args()
+    main(args)
