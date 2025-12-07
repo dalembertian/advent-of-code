583984f892858025837bfb8bb2a98ea6e5d222c6
Rubens Altimari <rubens@introspection.com.br>
2025-12-07 14:57:20 +0100

    Day 7, Part 2

--- 2025/07/solve.py
+++ 2025/07/solve.py
@@ -3,27 +3,67 @@
 
 import argparse
 
+from collections import defaultdict
+
 
 def main(args):
     levels = read_lines(args.filename)
 
     entry = levels.pop(0)
-    beams = set([entry.index('S')])
-    splits = 0
-    for level in levels:
-        new_beams = set()
-        while beams:
-            beam = beams.pop()
-            if level[beam] == '^':
-                splits += 1
-                new_beams.update([beam-1, beam+1])
-            else:
-                new_beams.add(beam)
-        beams = new_beams
-        print(beams)
+    first_beam = entry.index('S')
+    num_splits, num_timelines = count_timelines(first_beam, levels)
 
     # Correct: 1600
-    print(f'Part 1 - splits: {splits}')
+    print(f'Part 1 - splits: {num_splits}')
+
+    # Naïve approaches (correct but exponential time)    
+    # timelines = []
+    # find_timelines_recursive(first_beam, levels, timelines)
+    # print(f'Part 2 - timelines: {len(timelines)} or {count_timelines_recursive(first_beam, levels)}')
+
+    # Correct: 8632253783011
+    print(f'Part 2 - timelines: {num_timelines}')
+
+
+def count_timelines(beam, levels):
+    splits = defaultdict(int)
+    splits[beam] = 1
+    num_splits = 0
+    for level in levels:
+        new_splits = defaultdict(int)
+        for beam in splits.keys():
+            if level[beam] == '^':
+                new_splits[beam-1] += splits[beam]
+                new_splits[beam+1] += splits[beam]
+                num_splits += 1
+            else:
+                new_splits[beam] += splits[beam]
+        splits = new_splits
+    return num_splits, sum(splits.values())
+
+
+def count_timelines_recursive(beam, levels):
+    if not levels:
+        return 1
+    if levels[0][beam] == '^':
+        return count_timelines_recursive(beam+1, levels[1:]) + \
+               count_timelines_recursive(beam-1, levels[1:])
+    else:
+        return count_timelines_recursive(beam, levels[1:])
+
+
+def find_timelines_recursive(beam, levels, timelines, timeline = None):
+    if not levels:
+        timelines.append(timeline)
+        return
+    if not timeline:
+        timeline = []
+    timeline.append(beam)
+    if levels[0][beam] == '^':
+        find_timelines_recursive(beam+1, levels[1:], timelines, timeline[:])
+        find_timelines_recursive(beam-1, levels[1:], timelines, timeline)
+    else:
+        find_timelines_recursive(beam, levels[1:], timelines, timeline)
 
 
 def read_lines(filename):
