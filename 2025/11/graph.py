162a966d9239f3a6e69cb9bc257433787e62d4b5
Rubens Altimari <rubens@introspection.com.br>
2025-12-14 11:31:05 +0100

    Day 11, Part 2 - Just counting, no need to save paths

---
+++ 2025/11/graph.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from collections import deque
+
+
+INFINITE = float('inf')
+
+
+def map_previous_nodes(nodes, start):
+    # BFS traverse of nodes
+
+    for node in nodes.keys():
+        nodes[node].get('previous', []).clear()
+
+    V = deque([start])
+    while V:
+        v = V.popleft()
+        for node in nodes[v].get('next', []):
+            nodes[node].setdefault('previous', []).append(v)
+            V.append(node)
+
+
+def find_shortest_path(nodes, start, record_paths = False):
+    # Simplified Dijkstra for connections with cost zero
+    # (see 2024/16 or 2024/18 for a real Dijkstra implementation)
+    # Returns False if not all nodes are connected
+
+    # Mark all unvisited nodes with distance "infinite" from start - except start
+    unvisited = list(nodes.keys())
+    for node in unvisited:
+        nodes[node]['cost']  = INFINITE
+        nodes[node]['count'] = 0
+        nodes[node]['paths'] = []
+        nodes[node]['previous'] = []
+    nodes[start]['cost'] = 0
+    nodes[start]['count'] = 1
+    nodes[start].get('paths', []).append([])
+
+    while unvisited:
+        # Get unvisited node with shortest distance to start
+        unvisited.sort(key=lambda n: nodes[n]['cost'])
+        this = unvisited.pop(0)
+        if nodes[this]['cost'] == INFINITE:
+            break
+
+        # Visits every node linked to this STILL IN unvisited
+        for v in nodes[this].get('next', []):
+            if v in unvisited:
+                if nodes[v]['cost'] >= 0:
+                    nodes[v]['cost'] = 0
+                    nodes[v]['count'] += nodes[this]['count']
+                    nodes[v]['previous'].append(this)
+                    if record_paths:
+                        for path in nodes[this]['paths']:
+                            next_path = path[:]
+                            next_path.append(this)
+                            nodes[v]['paths'].append(next_path)
+    return False if unvisited else True
+
+
+def check_paths(nodes, end, required):
+    visited = []
+    current = required[:]
+    if check_current_path(nodes, end, visited, current):
+        return True
+    return False
+
+
+def check_current_path(nodes, end, visited, current):
+    # If already visited in this current check, it's cyclic
+    if end in current:
+        current.remove(end)
+    if not current:
+        return True
+
+    # If already visited previously, no need to check it again
+    if end in visited:
+        return False
+
+    current.append(start)
+    visited.append(start)
+    links = nodes[start].get('next', [])
+    if any([is_current_cyclic(nodes, link, visited, current) for link in links]):
+        return True
+
+    # Remove from current check before returning
+    current.remove(start)
+    return False
+
+
+def print_paths(nodes, end):
+    for path in nodes[end]['paths']:
+        print(path)
+
+
+def print_nodes(nodes):
+    for key in nodes.keys():
+        print(f'{key}: {nodes[key]}')
+    print()
+
+
+def is_cyclic(nodes, start):
+    # DFS check looking for cycles
+    visited = []
+    current = []
+    if is_current_cyclic(nodes, start, visited, current):
+        return True
+    return False
+
+
+def is_current_cyclic(nodes, start, visited, current):
+    # If already visited in this current check, it's cyclic
+    if start in current:
+        return True
+
+    # If already visited previously, no need to check it again
+    if start in visited:
+        return False
+
+    current.append(start)
+    visited.append(start)
+    links = nodes[start].get('next', [])
+    if any([is_current_cyclic(nodes, link, visited, current) for link in links]):
+        return True
+
+    # Remove from current check before returning
+    current.remove(start)
+    return False
--- 2025/11/solve.py
+++ 2025/11/solve.py
@@ -3,9 +3,9 @@
 
 import argparse
 
-from collections import deque, defaultdict
+from collections import defaultdict
 
-from dijkstra import *
+from graph import *
 
 
 def main(args):
@@ -13,58 +13,31 @@ def main(args):
 
     # Correct: 764
     # print(is_cyclic(nodes, 'you'))
-    explore_nodes(nodes, 'you')
+    map_previous_nodes(nodes, 'you')
     print(f'Part 1 - exit paths from \'you\': {len(nodes['out'].get('previous', []))}')
 
-    # Correct: 
-    nodes['out'] = {}
-    find_shortest_path(nodes, 'svr')
-    print_nodes(nodes)
-    print_paths(nodes, 'out')
+    # find_shortest_path(nodes, 'svr', True)
+    # print(f'{nodes['out'].get('count', 0)}')
+    # print_paths(nodes, 'out')
+    # print()
 
-    print(f'Part 2 - exit paths from \'svr\' passing by \'dac\' and \'fft\': {len(nodes['out'].get('previous', []))}')
+    # Incorrect: 4563341276580 (too low)
+    print(f'Part 2 - exit paths from \'svr\' passing by \'dac\' and \'fft\': {count_paths(nodes, ['dac', 'fft'])}')
 
-def explore_nodes(nodes, start):
-    # BFS traverse of nodes
-
-    for node in nodes.keys():
-        nodes[node].get('previous', []).clear()
-
-    V = deque([start])
-    while V:
-        v = V.popleft()
-        for node in nodes[v].get('next', []):
-            nodes[node].setdefault('previous', []).append(v)
-            V.append(node)
+    # Correct: 462444153119850
 
 
-def is_cyclic(nodes, start):
-    # DFS check looking for cycles
-    visited = []
-    current = []
-    if is_current_cyclic(nodes, start, visited, current):
-        return True
-    return False
-
-
-def is_current_cyclic(nodes, start, visited, current):
-    # If already visited in this current check, it's cyclic
-    if start in current:
-        return True
-
-    # If already visited previously, no need to check it again
-    if start in visited:
-        return False
-
-    current.append(start)
-    visited.append(start)
-    links = nodes[start].get('next', [])
-    if any([is_current_cyclic(nodes, link, visited, current) for link in links]):
-        return True
-
-    # Remove from current check before returning
-    current.remove(start)
-    return False
+def count_paths(nodes, passing_by):
+    first, second = passing_by
+    runs = [['svr', first, second, 'out'], ['svr', second, first, 'out']]
+    total = 0
+    for run in runs:
+        subtotal = 1
+        for i in range(3):
+            find_shortest_path(nodes, run[i])
+            subtotal *= nodes[run[i+1]].get('count', 0)
+        total += subtotal
+    return total
 
 
 def read_lines(filename):
